e2d4c7da157530f5ad19bf88cc334fa52e7c9317
enesokur <179an67@gmail.com>
2022-03-18 16:07:39 +0300

    Repo initialized

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: SafeArea(
+        child: Text("Init"),
+      ),
+    ),
+  ));
+}
